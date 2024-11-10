895bec42729a82f30ebe13a0769e9d1a612572d3
Vitor Diniz <vitordiniz25@gmail.com>
2024-11-10 12:37:08 -0300

    feat: auth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
-      <Button variant={"outline"}>This is a button</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
