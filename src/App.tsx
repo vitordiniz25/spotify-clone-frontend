522c6f768fa535eb703f33a40b69748a053040c0
Vitor Diniz <vitordiniz@Vitors-MacBook-Air.local>
2024-11-10 12:25:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl">Hello</h1>
+      <Button variant={"outline"}>This is a button</Button>
+    </>
+  );
+}
+
+export default App;
