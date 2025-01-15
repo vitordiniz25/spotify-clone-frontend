70143304bcff3cf4a9a70e0fa6bf38a439d5e62d
Vitor Diniz <vitordiniz25@gmail.com>
2025-01-14 23:17:27 -0300

    feat: main and left sidebar layout component

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,50 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+import LeftSidebar from "./components/LeftSidebar";
+
+function MainLayout() {
+  const isMobile = false;
+
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex-1 flex h-full overflow-hidden p-2"
+      >
+        {/* Left */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMobile ? 0 : 10}
+          maxSize={30}
+        >
+          <LeftSidebar />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Main content  */}
+        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* right */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={25}
+          collapsedSize={0}
+        >
+          friends activity component
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+}
+
+export default MainLayout;
---
+++ src/layout/components/LeftSidebar.tsx
@@ -0,0 +1,68 @@
+import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
+import { buttonVariants } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { SignedIn } from "@clerk/clerk-react";
+import { HomeIcon, Library, MessageCircle } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LeftSidebar() {
+  const isLoading = false;
+  return (
+    <div className="h-full flex flex-col gap-2">
+      {/* Navigation Menu  */}
+      <div className="rounded-lg bg-zinc-900 p-4">
+        <div className="space-y-2">
+          <Link
+            to={"/"}
+            className={cn(
+              buttonVariants({
+                variant: "ghost",
+                className: "w-full justify-start text-white hover:bg-zinc-800",
+              })
+            )}
+          >
+            <HomeIcon className="mr-2 size-5" />
+            <span className="hidden md:inline">Home</span>
+          </Link>
+
+          <SignedIn>
+            <Link
+              to={"/"}
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                  className:
+                    "w-full justify-start text-white hover:bg-zinc-800",
+                })
+              )}
+            >
+              <MessageCircle className="mr-2 size-5" />
+              <span className="hidden md:inline">Messages</span>
+            </Link>
+          </SignedIn>
+        </div>
+      </div>
+
+      {/* Library Section */}
+
+      <div className="flex-1 rounded-lg bg-zinc-900 p-4">
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center text-white px-2">
+            <Library className="size-5 mr-2" />
+            <span className="hidden md:inline">Playlists</span>
+          </div>
+        </div>
+
+        <ScrollArea className="h-[calc(100vh-300px)]">
+          <div className="space-y-2">
+            {isLoading ? <PlaylistSkeleton /> : "some music"}
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSidebar;
